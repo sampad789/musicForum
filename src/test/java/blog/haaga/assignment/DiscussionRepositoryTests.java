29812d91a8f7af7a7cbd024d69fc8ea37b195184
Sampurna Gautam <a1603177@myy.haaga-helia.fi>
2017-12-07 19:02:11 +0200

    fixed test issues and rest services

--- src/test/java/blog/haaga/assignment/DiscussionRepositoryTests.java
+++ src/test/java/blog/haaga/assignment/DiscussionRepositoryTests.java
@@ -1,5 +1,7 @@
 package blog.haaga.assignment;
 
+
+
 import java.util.List;
 
 import org.junit.Test;
@@ -11,7 +13,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.*;
 import blog.haaga.assignment.domain.Discussion;
 import blog.haaga.assignment.domain.DiscussionRepository;
-import blog.haaga.assignment.domain.GenreRepository;
+import blog.haaga.assignment.domain.Genre;
+
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -21,21 +24,26 @@ public class DiscussionRepositoryTests {
 
 	private DiscussionRepository drepository;
 
-	private GenreRepository grepository;
+	
 
 	@Test
 	public void findOneDiscussionByName() {
 		List<Discussion> discussions = drepository.findByName("josiah");
 		assertThat(discussions).hasSize(1);
+	}
+	@Test
+		public void findOneDiscussionById() {
+			List<Discussion> discussions = drepository.findById(2L);
+			assertThat(discussions).hasSize(1);
+
 
 	}
 
 	@Test
 	public void createNewDiscussion() {
-		Discussion newDiscussion = new Discussion("mati","jsingh@gmail.com","Cowboys From Hell","Pantera","This is just a demo for the topics to be used for the discussions","Great rock album",grepository.findByName("Alternative").get(0));
-		drepository.save(newDiscussion);
-		assertThat(newDiscussion.getId()).isNotNull();
-
+		Discussion newdis = new Discussion("Claudio","claoudfero@gmail.com","bob marley", "three wailers", "demotest","good music", new Genre ("Rock"));
+		drepository.save(newdis);
+		assertThat(newdis.getId()).isNotNull();
 	}
 
 }
